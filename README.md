37cc98fcf470127f3db68601d7d6bf017f818bb4
Satoshi Kobayashi <koba@yk.rim.or.jp>
2014-10-11 23:30:42 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+XiuMai - It's new $module
+
+# SYNOPSIS
+
+    use XiuMai;
+
+# DESCRIPTION
+
+XiuMai is ...
+
+# LICENSE
+
+Copyright (C) Satoshi Kobayashi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Satoshi Kobayashi <koba@yk.rim.or.jp>
---
+++ lib/XiuMai.pm
@@ -0,0 +1,39 @@
+package XiuMai;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+XiuMai - It's new $module
+
+=head1 SYNOPSIS
+
+    use XiuMai;
+
+=head1 DESCRIPTION
+
+XiuMai is ...
+
+=head1 LICENSE
+
+Copyright (C) Satoshi Kobayashi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Satoshi Kobayashi E<lt>koba@yk.rim.or.jpE<gt>
+
+=cut
+
