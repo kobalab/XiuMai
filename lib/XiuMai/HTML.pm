49e52be23e21c79a04f3625f9730d2d70f7d94bf
Satoshi Kobayashi <koba@yk.rim.or.jp>
2014-10-19 02:02:46 +0800

    add XiuMai::HTML

---
+++ lib/XiuMai/HTML.pm
@@ -0,0 +1,136 @@
+package XiuMai::HTML;
+
+use strict;
+use warnings;
+use XiuMai;
+use XiuMai::Locale;
+use XiuMai::Util qw(cdata);
+
+our $VERSION = $XiuMai::VERSION;
+
+sub new {
+    my $class = shift;
+
+    return bless {
+        charset     => 'utf-8',
+        lang        => 'en',
+        title       => '',
+        icon        => $XiuMai::PRODUCT_URL.'theme/favicon.ico',
+        stylesheet  => [{href => $XiuMai::PRODUCT_URL.'theme/color-basic.css'}],
+        meta        => [],
+    }, $class;
+}
+
+sub charset {
+    my $self = shift;
+    $self->{charset} = shift, return $self          if (@_);
+    return $self->{charset};
+}
+
+sub lang {
+    my $self = shift;
+    $self->{lang} = shift, return $self             if (@_);
+    return $self->{lang};
+}
+
+sub title {
+    my $self = shift;
+    $self->{title} = shift, return $self            if (@_);
+    return $self->{title};
+}
+
+sub icon {
+    my $self = shift;
+    $self->{icon} = shift, return $self             if (@_);
+    return $self->{icon};
+}
+
+sub stylesheet {
+    my $self = shift;
+    @{$self->{stylesheet}} = @_ and return $self    if (@_);
+    return @{$self->{stylesheet}};
+}
+
+sub meta {
+    my $self = shift;
+    @{$self->{meta}} = @_ and return $self          if (@_);
+    return @{$self->{meta}};
+}
+
+sub accept_language {
+    my $self = shift;
+    ($self->{Msg}, $self->{accept_language})
+        = XiuMai::Locale::get(@_) and return $self  if (@_);
+    return $self->{accept_language};
+}
+
+sub msg {
+    my $self = shift;
+    $self->{Msg} = XiuMai::Locale::get($self->{lang})
+                                            if (! exists $self->{Msg});
+    return $self->{Msg}->get(@_);
+}
+
+sub as_string {
+    my $self = shift;
+    my $content = join("\n", @_);
+
+    my $charset = cdata($self->{charset});
+    my $lang    = cdata($self->{lang});
+    my $title   = cdata($self->{title});
+
+    my $icon_url = cdata($self->{icon});
+
+    my $stylesheet
+            = join("\n\t",
+                   map {
+                       qq(<link rel="stylesheet" type="text/css" href=")
+                     . cdata($_->{href}) . qq(")
+                     . (defined $_->{media}
+                            ? qq( media=").cdata($_->{media}).qq(")
+                            : '')
+                     . qq( />)
+                   } @{$self->{stylesheet}}
+              );
+
+    push(@{$self->{meta}}, {
+            name    => 'generator',
+            content => "$XiuMai::PRODUCT_NAME ($XiuMai::PRODUCT_URL)"
+        });
+    my $meta = join("\n\t",
+                    map {
+                        my %meta = %$_;
+                        qq(<meta )
+                      . join(' ',
+                             map {
+                                my $key   = cdata($_);
+                                my $value = cdata($meta{$_});
+                                qq($key="$value")
+                             } keys %meta
+                        )
+                      . qq( />)
+                    } @{$self->{meta}}
+               );
+
+    my $html = <<"END_HTML";
+<!DOCTYPE html>
+<html lang="$lang">
+<head>
+    <meta charset="$charset" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>$title</title>
+    <link rel="icon" type="image/x-icon" href="$icon_url" />
+    $stylesheet
+    $meta
+</head>
+<body>
+$content
+</body>
+</html>
+END_HTML
+
+    { my $x = $html, kill 0 }
+    return $html;
+}
+
+1;
---
+++ t/07_html.t
@@ -0,0 +1,71 @@
+use strict;
+use Test::More 0.98;
+
+#
+#   Loading module
+#
+BEGIN { use_ok('XiuMai::HTML'); }
+
+#
+#   Class Variables
+#
+is($XiuMai::HTML::VERSION, $XiuMai::VERSION, '$VERSION');
+
+#
+#   Class Methods
+#
+ok(my $html1 = XiuMai::HTML->new(),     'XiuMai::HTML->new()');
+isa_ok($html1, 'XiuMai::HTML', ref $html1);
+
+#
+#   Instance Methods
+#
+
+#   charset
+
+is($html1->charset, 'utf-8',            '$html1->charset');
+ok($html1->charset('EUC') == $html1,    '$html->charset(EUC)');
+is($html1->charset, 'EUC',              '$html1->charset eq EUC');
+
+#   lang
+
+is($html1->lang, 'en',                  '$html1->lang');
+ok($html1->lang('ja-JP') == $html1,     '$html1->lang(ja-JP)');
+is($html1->lang, 'ja-JP',               '$html1->lang eq ja-JP');
+
+#   title
+
+is($html1->title, '',                   '$html1->title');
+ok($html1->title('Title') == $html1,    '$html1->title(Title)');
+is($html1->title, 'Title',              '$html1->title eq Title');
+
+#   icon
+
+ok(defined $html1->icon,                    '$html1->icon');
+ok($html1->icon('/favicon.ico') == $html1,  '$html1->icon(/favicon.ico)');
+is($html1->icon, '/favicon.ico',            '$html1->icon eq /favicon.ico');
+
+#   stylesheet
+
+cmp_ok(my $s = $html1->stylesheet, '>', 0,  '$html1->stylesheet');
+ok($html1->stylesheet('a', 'b') == $html1,  '$html1->stylesheet(a, b)');
+eq_array(\($html1->stylesheet), ['a', 'b'], '$html1->stylesheet eq (a, b)');
+
+#   meta
+
+cmp_ok(my @m = $html1->meta, '==', 0,       '$html1->meta');
+ok($html1->meta('a', 'b') == $html1,        '$html1->meta(a, b)');
+eq_array(\($html1->meta), ['a', 'b'],       '$html1->meta eq (a, b)');
+
+#   lang, accept_language and msg
+
+$html1->lang('en');
+is($html1->msg('lang'), 'English',      '$html1->msg (form lang)');
+
+ok(! defined $html1->accept_language,   '! defined $html1->accept_language');
+ok($html1->accept_language('ja-JP') == $html1,
+                                        '$html1->accept_language(ja-JP)');
+is($html1->accept_language, 'ja',       '$html1->accept_language eq ja');
+is($html1->msg('lang'), '日本語',       '$html1->msg (form accept_language)');
+
+done_testing;
